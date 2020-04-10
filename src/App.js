75290b1b5b4ab546ea3a847962008b6cad9f3c53
Frank Moran <info@francisfrankmoran.com>
2020-04-10 01:02:08 -0400

    sass setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
+import "./App.sass";
 
 function App() {
   return (
