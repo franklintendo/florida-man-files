b7f3e02f2086e1f9bbec1fa27070f54ed82d543e
Frank Moran <info@francisfrankmoran.com>
2020-04-10 00:55:12 -0400

    setup

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+function Home() {
+    return(
+        <Container fluid>
+            <Row>
+                <Col>
+                    Whats up
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default Home;
