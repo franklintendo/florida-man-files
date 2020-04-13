c25d453d0db5e33fe98b33f5f6d65de7234a3a98
Frank Moran <info@francisfrankmoran.com>
2020-04-12 20:07:54 -0400

    FL image, background, home layout setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,22 @@ import { Container, Row, Col } from "react-bootstrap";
 
 function Home() {
     return(
-        <Container fluid>
-            <Row>
-                <Col>
-                    Whats up
+        <Container fluid className="h-100 w-100 florida__home--container">
+            <Row className="h-100 justify-content-center">
+
+                {/* <Col className="col-12 col-md-8 col-lg-4 florida__mugshot--container">
+                    <img src={require('../img/mugshot/berton-strong.png')} alt="Berton Strong" className="florida__mugshot--img rounded-circle my-auto d-block" />
+
+                    <img src={require('../img/mugshot/berton-strong.png')} alt="Berton Strong" className="florida__mugshot--img rounded-circle my-auto mx-auto d-block" />
+                </Col> */}
+                <Col className="col-auto my-auto mx-auto florida__mugshot--container">
+                    <img src={require('../img/mugshot/berton-strong.png')} alt="Berton Strong" className="florida__mugshot--img rounded-circle" />
+                    <br/>
+                    <img src={require('../img/mugshot/berton-strong.png')} alt="Berton Strong" className="florida__mugshot--img rounded-circle" />
+                </Col>
+
+                <Col className="h-100 col-12 col-md-8 col-lg-4 florida__home--content">
+                    &nbsp;
                 </Col>
             </Row>
         </Container>
