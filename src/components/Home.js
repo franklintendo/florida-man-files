7adabf66f95cb5a9cc6432ec43730e0cc112154f
Frank Moran <info@francisfrankmoran.com>
2020-04-12 20:39:57 -0400

    all shots

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,8 +13,20 @@ function Home() {
                 </Col> */}
                 <Col className="col-auto my-auto mx-auto florida__mugshot--container">
                     <img src={require('../img/mugshot/berton-strong.png')} alt="Berton Strong" className="florida__mugshot--img rounded-circle" />
-                    <br/>
-                    <img src={require('../img/mugshot/berton-strong.png')} alt="Berton Strong" className="florida__mugshot--img rounded-circle" />
+                    
+                    <img src={require('../img/mugshot/ben-jenkins.png')} alt="Ben Jenkins" className="florida__mugshot--img rounded-circle" />
+
+                    <img src={require('../img/mugshot/albert-alvarez.png')} alt="Albert Alvarez" className="florida__mugshot--img rounded-circle" />
+
+                    <img src={require('../img/mugshot/joel-jennings.png')} alt="Joel Jennings" className="florida__mugshot--img rounded-circle" />
+
+                    <img src={require('../img/mugshot/carl-skippins.png')} alt="Carl Skippins" className="florida__mugshot--img rounded-circle" />
+
+                    <img src={require('../img/mugshot/herman-douglas.png')} alt="Herman Douglas" className="florida__mugshot--img rounded-circle" />
+
+                    <img src={require('../img/mugshot/dewey-fleming.png')} alt="Dewey Fleming" className="florida__mugshot--img rounded-circle" />
+
+                    <img src={require('../img/mugshot/donald-schmidt.png')} alt="Donald Schmidt" className="florida__mugshot--img rounded-circle" />
                 </Col>
 
                 <Col className="h-100 col-12 col-md-8 col-lg-4 florida__home--content">
