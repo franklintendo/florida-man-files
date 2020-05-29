884e5e8e1cf81b7b61c873f9f86d4ce2adf5f8ae
Frank Moran <info@francisfrankmoran.com>
2020-05-29 12:56:10 -0400

    onclick hamburger background transition

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
 function Hamburger() {
+
+    const showSidebar = () => {
+      let hamburger = document.querySelector(".hamburger-menu").classList;
+      let body = document.body;
+
+      if (hamburger.contains("expanded")) {
+        hamburger.remove("expanded");
+        body.classList.remove("remove-scroll");
+      } else {
+        hamburger.add("expanded");
+        body.classList.add("remove-scroll");
+      }
+    }
+
     return (
-      <div class="hamburger-menu pr-3 pt-3">
-          <i class="fas fa-bars"></i>
+      <div class="hamburger-menu">
+          <i class="px-3 py-3 fas fa-bars" onClick={showSidebar}></i>
       </div>
     );
   }
