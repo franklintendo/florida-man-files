6e5b2aa43b007619a5f4c4667edc7a9fbb6a0050
Frank Moran <info@francisfrankmoran.com>
2020-07-04 13:42:42 -0400

    phone positioning during transition closing, play btn display

--- src/components/Phone.js
+++ src/components/Phone.js
@@ -4,10 +4,11 @@ function Phone() {
 
     const playVideo = () => {
         let phoneDiv = document.querySelector(".florida__case-file--phone").classList;
-        let phonePlayBtn = document.querySelector(".florida__case-file--phone-play").classList;
-        let phoneCloseBtn = document.querySelector(".florida__case-file--phone-close").classList;
+        let phonePlayBtn = document.querySelector(".florida__case-file--phone-play-btn").classList;
+        let phoneCloseBtn = document.querySelector(".florida__case-file--phone-close-btn").classList;
 
         phoneDiv.add("playing");
+        phoneDiv.remove("closing");
         phonePlayBtn.add("d-none");
         phoneCloseBtn.remove("d-none");
 
@@ -15,21 +16,21 @@ function Phone() {
 
     const closeVideo = () => {
         let phoneDiv = document.querySelector(".florida__case-file--phone").classList;
-        let phonePlayBtn = document.querySelector(".florida__case-file--phone-play").classList;
-
-        let phoneCloseBtn = document.querySelector(".florida__case-file--phone-close").classList;
+        let phonePlayBtn = document.querySelector(".florida__case-file--phone-play-btn").classList;
+        let phoneCloseBtn = document.querySelector(".florida__case-file--phone-close-btn").classList;
 
         phoneDiv.remove("playing");
-        phonePlayBtn.remove("d-none");
+        phoneDiv.add("closing");
         phoneCloseBtn.add("d-none");
+        setTimeout(() => { phonePlayBtn.remove("d-none"); }, 500);
 
     }
 
     return(
         
         <div className="florida__case-file--phone">
-            <p onClick={closeVideo} className="florida__case-file--phone-close d-none">x</p>
-            <img onClick={playVideo} className="florida__case-file--phone-play" src={require('../img/play-btn.png')} alt="play button" />
+            <p onClick={closeVideo} className="florida__case-file--phone-close-btn d-none">x</p>
+            <img onClick={playVideo} className="florida__case-file--phone-play-btn" src={require('../img/play-btn.png')} alt="play button" />
             <img className="florida__case-file--phone-img" src={require('../img/phone.png')} alt="phone" />
         </div>          
             
