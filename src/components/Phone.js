3d6b9265f47daf2967c4f58643d3e858135edbb9
Frank Moran <info@francisfrankmoran.com>
2020-07-04 23:00:32 -0400

    close by clicking on background active phone

--- src/components/Phone.js
+++ src/components/Phone.js
@@ -6,11 +6,13 @@ function Phone() {
         let phoneDiv = document.querySelector(".florida__case-file--phone").classList;
         let phonePlayBtn = document.querySelector(".florida__case-file--phone-play-btn").classList;
         let phoneCloseBtn = document.querySelector(".florida__case-file--phone-close-btn").classList;
+        let overlayBackground = document.querySelector(".close-background").classList;
 
         phoneDiv.add("playing");
         phoneDiv.remove("closing");
         phonePlayBtn.add("d-none");
         phoneCloseBtn.remove("d-none");
+        overlayBackground.remove("d-none");
 
     }
 
@@ -18,10 +20,12 @@ function Phone() {
         let phoneDiv = document.querySelector(".florida__case-file--phone").classList;
         let phonePlayBtn = document.querySelector(".florida__case-file--phone-play-btn").classList;
         let phoneCloseBtn = document.querySelector(".florida__case-file--phone-close-btn").classList;
+        let overlayBackground = document.querySelector(".close-background").classList;
 
         phoneDiv.remove("playing");
         phoneDiv.add("closing");
         phoneCloseBtn.add("d-none");
+        overlayBackground.add("d-none");
         setTimeout(() => { phonePlayBtn.remove("d-none"); }, 500);
 
     }
@@ -32,6 +36,9 @@ function Phone() {
             <p onClick={closeVideo} className="florida__case-file--phone-close-btn d-none">x</p>
             <img onClick={playVideo} className="florida__case-file--phone-play-btn" src={require('../img/play-btn.png')} alt="play button" />
             <img className="florida__case-file--phone-img" src={require('../img/phone.png')} alt="phone" />
+            <div className="close-background w-100 h-100 d-none" onClick={closeVideo}>
+            
+          </div>
         </div>          
             
     )
