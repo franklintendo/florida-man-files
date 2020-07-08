0478934d5d06a12a6bfc520a60f58de2ac45725f
Frank Moran <info@francisfrankmoran.com>
2020-07-08 15:00:40 -0400

    more media queries, new photo img for sm

--- src/components/Mugshot.js
+++ src/components/Mugshot.js
@@ -5,7 +5,9 @@ function Mugshot({image}) {
     return(
         
         <div className="florida__case-file--photo">
-            <img className="florida__case-file--photo-container" src={require('../img/photo-container.png')} alt="Polaroid" />
+            <img className="florida__case-file--photo-container d-none d-md-block" src={require('../img/photo-container.png')} alt="Polaroid" />
+
+            <img className="florida__case-file--photo-container d-block d-md-none" src={require('../img/photo-container-no-clip.png')} alt="Polaroid" />
 
             <img name={image.link} src={require(`../img/mugshot/${image.link}.png`)} alt={image.name} className={`florida__case-file--photo-mugshot`} />
 
